36bada0308fa60669a338161a9603e81e67f3f3f
Rich Samartino <rich@detroitcitycentral.com>
2017-08-16 21:57:37 -0400

    updated symfony to 3.3; ran composer update; changed Gleaners report

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,12 +17,12 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
 			new FOS\UserBundle\FOSUserBundle(),
-			//new FPN\TagBundle\FPNTagBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle;
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
 			$bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
